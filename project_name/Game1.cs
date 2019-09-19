500f1e442023783af5c9759a6a7d5a037d26141a
Mike <mike@prime31.com>
2019-09-19 16:42:47 -0700

    minor cleanup of Game

--- project_name/Game1.cs
+++ project_name/Game1.cs
@@ -6,24 +6,29 @@ namespace project_name
     class Game1 : Core
     {
         public Game1() : base()
-        {}
+        {
+			// uncomment this line for scaled pixel art games
+			//Environment.SetEnvironmentVariable("FNA_OPENGL_BACKBUFFER_SCALE_NEAREST", "1");
+        }
 
         override protected void Initialize()
         {
             base.Initialize();
-			
-			// uncomment this line for scaled pixel art games
-			//Environment.SetEnvironmentVariable("FNA_OPENGL_BACKBUFFER_SCALE_NEAREST", "1");
 			
 #if DEBUG
             System.Diagnostics.Debug.Listeners.Add(new System.Diagnostics.TextWriterTraceListener(System.Console.Out));
 #endif
 
             Scene = new DefaultScene();
-            
+
             // optionally render Nez in an ImGui window
 			var imGuiManager = new ImGuiManager();
 			Core.RegisterGlobalManager(imGuiManager);
+
+#if DEBUG
+			// optionally load up ImGui DLL if not using the above setup so that its command gets loaded in the DebugConsole
+			//System.Reflection.Assembly.Load("Nez.ImGui")
+#endif
         }
     }
 }
