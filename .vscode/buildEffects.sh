b4f596057475510ac7d6fb1611874cdcb2a5d256
prime31 <mike@prime31.com>
2020-04-01 21:20:26 -0700

    64bit wine and fxc

--- .vscode/buildEffects.sh
+++ .vscode/buildEffects.sh
@@ -13,7 +13,7 @@ do
     export WINEDEBUG=fixme-all,err-all
 
     # Build the effect
-    wine ~/.wine/drive_c/Program\ Files\ \(x86\)/Microsoft\ DirectX\ SDK\ \(June\ 2010\)/Utilities/bin/x86/fxc.exe\
+    wine64 ~/.wine/drive_c/Program\ Files\ \(x86\)/Microsoft\ DirectX\ SDK\ \(June\ 2010\)/Utilities/bin/x64/fxc.exe\
      /T fx_2_0 $file /Fo "`dirname $file`/`basename $file .fx`.fxb"
 
     echo ""
